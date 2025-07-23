9a612df40664eaf364421485d36123eb4f52548a
Shesha234 <60121377+Shesha234@users.noreply.github.com>
2025-07-23 13:19:15 +0530

    section3.spec.js

--- section3.spec.js
+++ section3.spec.js
@@ -3,9 +3,10 @@ import { test, expect} from '@playwright/test';
 
 test('has title', async ({ page }) => {
     const url='https://practicetestautomation.com'
-    await page.goto(url+'/practice-test-login/'); // Replace with your login page URL
+    await page.goto(url+'/practice-test-login/');
     await page.locator('#username').fill('student');
     await page.locator('#password').fill('Password123');
+    await page.locator('#submit').hover();
     await page.locator('#submit').click();
     await page.waitForURL(url+'/logged-in-successfully/');
     const logoutButton = page.getByText('Log out');
